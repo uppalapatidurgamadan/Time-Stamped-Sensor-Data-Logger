4187444d3d8b5418a610a79f213ec9456882c534
UPPALAPATI DURGA MADAN <uppalapatidurgamadan@gmail.com>
2026-06-26 17:37:47 +0530

    Delete mini_project/hex directory

--- mini_project/hex/types .h
+++
@@ -1,5 +0,0 @@
-typedef int s32;
-typedef unsigned int u32;
-typedef char s8;
-typedef unsigned char u8;
-
--- mini_project/hex/uart .h
+++
@@ -1,7 +0,0 @@
-void inituart(void);
-void Txuart(unsigned char );
-unsigned char Rxuart(void);
-void uartTxstring(char *);
-void Txint(int);
-void Txfloat(float);
-
